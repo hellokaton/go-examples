662fe6db5e43a4881d0e1eaed98a39ca11dbf33f
biezhi <biezhi.me@gmail.com>
2017-04-03 17:31:50 +0800

    update pointer code

--- basic/L_pointers.go
+++ basic/L_pointers.go
@@ -20,6 +20,17 @@ fmt.Println(*p) // 通过指针 p 读取 i
 
 与 C 不同，Go 没有指针运算。
 */
+
+/*
+& 取一个变量的地址
+* 取一个指针变量所指向的地址的值
+所谓指针就是一个指向（存储）特定变量地址的变量
+*/
+
+func change(x *int)  {
+	*x = 1024
+}
+
 func main() {
 	i, j := 42, 2701
 	p := &i         // 指向变量i
@@ -30,4 +41,27 @@ func main() {
 	p = &j         // 指向变量j
 	*p = *p / 37   // 通过指针修改j的数据
 	fmt.Println("变量j的值:", j) // 查看j的新值
+
+
+	// 指针
+	x := 10
+
+	// 指针变量
+	var x_ptr *int = &x
+
+	var x2 = &x
+
+	fmt.Println(x)        	// 10
+	fmt.Println(x_ptr)    	// x的地址
+	fmt.Println(&x)        	// x的地址
+	fmt.Println(x2)        	// x的地址
+	fmt.Println(*x_ptr)    	// 通过指针变量输出x的值
+	fmt.Println(*x2)    	// 通过指针变量输出x的值
+	fmt.Println(&x_ptr)		// 取x_ptr指针变量的地址
+	fmt.Println(*&x_ptr)	// *temp =  &x_ptr	=> &x => x的地址
+
+	num := 10
+	fmt.Println(num)
+	change(&num)
+	fmt.Println(num)
 }
